39a47f26173b68dc503cfaf161b9c1c242a68e9d
LuccasAguiarLCC <luccasaguiarlcc@gmail.com>
2023-06-04 23:56:14 -0300

    finalizacao

--- vagas.js
+++ vagas.js
@@ -3,8 +3,8 @@ let jobsList = [];
 function showJobs() {
   let vacancyText = jobsList.reduce(function (finalText, vacancy, index) {
     finalText += `${index}. `;
-    finalText += vacancy.name;
-    finalText += `(${vacancy.candidates.length} candidatos)\n`;
+    finalText += vacancy.jobName;
+    finalText += ` (${vacancy.candidates.length} candidatos)\n`;
     return finalText;
   }, "");
 
@@ -50,3 +50,87 @@ function seeJobs() {
   Quantidade de candidatos: ${vacancy.candidates.length}
   Candidatos inscritos: ${candidatesText}`);
 }
+
+function subCandidate() {
+  let candidate = prompt("Qual é o nome do candidato?");
+  let index = prompt(
+    "Qual o índice da vaga para a qual o candidato deseja se inscrever?"
+  );
+  let vacancy = jobsList[index];
+
+  let answer =
+    confirm(`Deseja inscrever o candidato ${candidate} na vaga ${index}?
+  Nome: ${vacancy.jobName}
+  Descrição: ${vacancy.jobDescription}
+  Data limite: ${vacancy.jobData}`);
+
+  if (answer) {
+    vacancy.candidates.push(candidate);
+    alert(`Candidato ${candidate} foi inscrito com sucesso!`);
+  } else {
+    alert("Candidato não inscrito na vaga.");
+  }
+}
+
+function deleteJobVac() {
+  let index = prompt("Informe o índice da vaga que deseja excluir:");
+  let vacancy = jobsList[index];
+
+  let answer = confirm(`Você tem certeza que deseja excluir a vaga ${index}?
+  Nome: ${vacancy.jobName}
+  Descrição: ${vacancy.jobDescription}
+  Data limite: ${vacancy.jobData}`);
+
+  if (answer) {
+    jobsList.splice(index, 1);
+    alert("Vaga excluída.");
+  } else {
+    alert("A operação foi cancelada.");
+  }
+}
+
+function showMenu() {
+  let options = prompt(`Bem vindo ao cadastro de vagas de emprego!
+  Por favor, escolha uma opção:
+  [ 1 ] Listar vagas disponíveis
+  [ 2 ] Criar nova vaga
+  [ 3 ] Visualizar uma vaga
+  [ 4 ] Inscrever candidato em uma vaga
+  [ 5 ] Excluir uma vaga
+  [ 6 ] Sair`);
+
+  return options;
+}
+
+function exe() {
+  let option = "";
+
+  do {
+    option = showMenu();
+
+    switch (option) {
+      case "1":
+        showJobs();
+        break;
+      case "2":
+        newJob();
+        break;
+      case "3":
+        seeJobs();
+        break;
+      case "4":
+        subCandidate();
+        break;
+      case "5":
+        deleteJobVac();
+        break;
+      case "6":
+        alert("Encerrando...");
+        break;
+      default:
+        alert("algo deu errado!\n Atualize a página e tente novamente.");
+    }
+  } while (option !== "6");
+}
+
+exe();
